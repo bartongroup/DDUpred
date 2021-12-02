2e72eeea95418fb2fdb76d4c813fca75d8eede0c
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2021-12-02 09:14:48 +0000

    Some more work

--- R/io.R
+++ R/io.R
@@ -1,4 +1,7 @@
-import_data <- function(file) {
-  read_csv(file, col_types = cols(), na=c("", "NA", "n/a", "N/A", "None"), guess_max = 10000)
+import_data <- function(file, verbose=FALSE) {
+  if(verbose) cat(paste("Reading data from", file, "\n"))
+  d <- read_csv(file, col_types = cols(), na=c("", "NA", "n/a", "N/A", "None"), guess_max = 10000, progress = FALSE)
+  if(verbose) cat(paste("   ", nrow(d), "rows,", ncol(d), "columns\n"))
+  return(d)
 }
 
--- R/main.R
+++ R/main.R
@@ -1,7 +1,8 @@
 library(fitdistrplus)
 library(randomForest)
 library(tidyverse)
-source("R/processing.R")
+source("R/process.R")
+source("R/variables.R")
 source("R/io.R")
 source("R/grouping.R")
 source("R/random_forest.R")
@@ -10,22 +11,28 @@ select <- dplyr::select
 
 EXP_VARIABLES <- c("Exp_CHI-LOGD", "Exp_GSK-ChromLogD", "Exp_PAMPA-Papp", "Exp_realSOL_7.4", "log Exp_CLi Heps mouse", "log Exp_CLi Mics mouse", "log Exp_PPB mouse")
 
-ddu_prediction <- function(train_file, info_file, test_file) {
-  train_raw <- import_data(train_file)
-  package_info <- read_csv(info_file, show_col_types = FALSE)
+ddu_prediction <- function(train_file, info_file, test_file, verbose=TRUE) {
   
-  train_set <- process_raw(train_raw, package_info, 
+  train_raw <- import_data(train_file, verbose)
+  package_info <- read_csv(info_file, show_col_types = FALSE, progress = FALSE)
+  
+  train_set <- process_training_data(train_raw, package_info, 
     cols_id = "Name",
     cols_remove = "Molecule Name",
     cols_pka = c("G+_pH7.4_S+Acidic_pKa", "G+_pH7.4_S+Mixed_pKa", "G+_pH7.4_S+Basic_pKa"),
     cols_nolog = c("G+_pH7.4_S+pH_Satd", "Exp_pKa-Sirius PKA1"),
-    relevel_moka_status = TRUE
+    verbose = verbose
   )
   
-  test_raw <- import_data(test_file)
-  test_set <- process_test(test_raw, train_set$variables, cols_id = "Name", cols_remove = c("G+_pH7.4_SupSatn", "G+_pH7.4_BBB_Filter", "G+_pH7.4_AcidAtoms", "G+_pH7.4_BaseAtoms"))
-  
-  pred <- predict_new_rf_exps(train_set, EXP_VARIABLES, test_set$tab)
+  test_raw <- import_data(test_file, verbose)
+
+  test_set <- process_test_data(test_raw, train_set$variables,
+    cols_id = "Name",
+    cols_remove = c("G+_pH7.4_SupSatn", "G+_pH7.4_BBB_Filter", "G+_pH7.4_AcidAtoms", "G+_pH7.4_BaseAtoms"),
+    verbose = verbose
+  )
+
+  #predict_new_rf_exps(train_set, EXP_VARIABLES, test_set$tab)
 }
 
 
