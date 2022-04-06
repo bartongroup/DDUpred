9464907f287dd4af6a4840b100e101aee2531a15
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2022-04-06 12:11:05 +0100

    Adding moka file; fixing lombardo rename

--- bin/mergeph.R
+++ bin/mergeph.R
@@ -13,6 +13,8 @@ option_list <- list(
               help = "Descriptor data Excel file, contains all data, including pH7.4 data."),
   make_option(c("-p", "--ph2-file"), action = "store", default = NA, type = 'character',
               help = "Excel file with pH2 data."),
+  make_option(c("-m", "--moka-file"), action = "store", default = NULL, type = 'character',
+              help = "Excel file with Moka designations data (optional)."),
   make_option(c("-o", "--output-file"), action = "store", default = NA, type = 'character',
               help = "Output file name (CVS file).")
 )
@@ -25,7 +27,7 @@ if (is.na(opt$`descriptor-file`) | is.na(opt$`ph2-file`) | is.na(opt$`output-fil
   stop("Option(s) missing")
 }
 
-merged <- merge_ph(opt$`descriptor-file`, opt$`ph2-file`, verbose = TRUE)
+merged <- merge_ph(opt$`descriptor-file`, opt$`ph2-file`, opt$`moka-file`, verbose = TRUE)
 
 cat(paste("\nWriting output file", opt$`output-file`, "\n"))
 write_csv(merged, opt$`output-file`)
